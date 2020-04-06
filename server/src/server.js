704905d00509c173a57bdebf731a080e1cc65822
Kishore <kishore.p.panduri@oracle.com>
2020-04-06 16:28:56 -0500

    Top three most common words

--- server/src/server.js
+++ server/src/server.js
@@ -26,6 +26,25 @@ app.listen(port, () =>
   console.log(`App is listening on port ${port}.`)
 );
 
+
+function getTopWords(data, cutOff) {
+    // Replace . with empty String
+    let cleanData = data.replace(/[\.]/gm,""),        
+     words = cleanData.split(' '),
+     frequencies = {},
+     word, i;
+    // Build map with words and their frequencies 
+    for( i=0; i<words.length; i++ ) {
+      word = words[i];
+      frequencies[word] = frequencies[word] || 0;
+      frequencies[word]++;
+    }
+    words = Object.keys( frequencies );
+    // Return top cutOff=3 common words 
+    return words.sort(function (a,b) { return frequencies[b] -frequencies[a];}).slice(0,cutOff).toString();
+  }
+  
+
 // Upload text file
 app.post('/upload', async (req, res) => {
     try {
@@ -39,28 +58,31 @@ app.post('/upload', async (req, res) => {
             let textFile = req.files.textFile;
             //Read text file data
             let data=textFile.data.toString('utf8');
+
             
             // Number of words in text file.
             let noOfWords=data.split(" ").length;
 
             // Number of letters in text file.
-            let rxLetters = /[a-z]/gi;
+            let rxLetters = /[a-z]/gmi;
             let noOfLetters = data.match(rxLetters) ? data.match(rxLetters).length:0; 
            
             //Number of symbols in text file.
-            let rxSymbols = /[^\w\s]/g;
+            let rxSymbols = /[^\w\s]/gm;
             let noOfSymbols = data.match(rxSymbols) ? data.match(rxSymbols).length:0; 
+
+            //Top three most common words             
+            let topWords = getTopWords( data, 3);
 
             //send response
             res.send({
                 status: true,
                 message: 'File is uploaded',
                 data: {
-                    name: textFile.name,
-                    mimetype: textFile.mimetype,
-                    size: textFile.size,
+                    name: textFile.name,                    
                     noOfWords:noOfWords,
-                    noOfSymbols:noOfSymbols
+                    noOfSymbols:noOfSymbols,
+                    topWords:topWords
                 }
             });
         }
