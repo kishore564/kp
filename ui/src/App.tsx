99c5704c458841c7ba4dabf67aadbe0bd0c123cc
Kishore <kishore.p.panduri@oracle.com>
2020-04-06 15:13:59 -0500

    Basic UI with File Upload

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
 import './App.css';
+import { Upload, message } from 'antd';
+import { InboxOutlined } from '@ant-design/icons';
+
+const { Dragger} = Upload as any;
+
+const props = {
+  name: 'textFile',
+  multiple: false,
+  action: 'http://localhost:8005/upload',
+  onChange(info:any) {
+    const { status } = info.file;
+    if (status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
+
 
 function App() {
   return (
-    <div className="App">     
+    <div className="App">  
+    <Dragger {...props}>
+    <p className="ant-upload-drag-icon">
+      <InboxOutlined />
+    </p>
+    <p className="ant-upload-text">Click or drag file to this area to upload</p>
+    <p className="ant-upload-hint">
+      Support for a single or bulk upload. Strictly prohibit from uploading company data or other
+      band files
+    </p>
+  </Dragger>
     </div>
   );
 }
