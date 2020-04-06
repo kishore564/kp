e63756b3d7fc1fe8ca29edba0eea2b23bb4643de
Kishore <kishore.p.panduri@oracle.com>
2020-04-06 14:41:18 -0500

    Post API to upload text file

--- server/src/server.js
+++ server/src/server.js
@@ -26,3 +26,34 @@ app.listen(port, () =>
   console.log(`App is listening on port ${port}.`)
 );
 
+// Upload text file
+app.post('/upload', async (req, res) => {
+    try {
+        if(!req.files) {
+            res.send({
+                status: false,
+                message: 'No file uploaded'
+            });
+        } else {
+            //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+            let textFile = req.files.textFile;
+            
+            //Use the mv() method to place the file in upload directory (i.e. "uploads")
+            textFile.mv('./uploads/' + textFile.name);
+
+            //send response
+            res.send({
+                status: true,
+                message: 'File is uploaded',
+                data: {
+                    name: textFile.name,
+                    mimetype: textFile.mimetype,
+                    size: textFile.size
+                }
+            });
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
